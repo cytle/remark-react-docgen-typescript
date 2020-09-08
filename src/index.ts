b28878be17eb2c7ae4a78c927096c123f237ae12
无码 <canyoutle@gmail.com>
2020-09-08 21:40:27 +0800

    chore: unittest

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ const reactDocgenTypescript: Plugin<[ReactDocgenTypescriptOptions?]> =
     const parser = fileParser || withDefaultConfig(parseOptions);
     return (tree, file) => {
       visit(tree, 'link', (node: Link, index, parent) => {
+        /* istanbul ignore next */
         if (node.title && node.title.startsWith('react-docgen-typescript:')) {
           const docs = render(parser.parse(path.resolve(file.dirname, node.url)));
           parent.children.splice(index, 1, docs);
